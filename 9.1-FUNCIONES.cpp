32cb4c7459cdae209e6fe29584a0f3257a812974
sopita <angelmedina121215@gmail.com>
2025-01-20 23:14:28 -0600

    Add files via upload

---
+++ 9.1-FUNCIONES.cpp
@@ -0,0 +1,15 @@
+//AQUI VA EL CUERPO DE LA FUNCION IMC
+#include "9.1.2.-FUNCIONES.h"
+
+int add(int num5, int num6) {
+    return num5 + num6;
+}
+int substract(int num5, int num6) {
+    return num5 - num6;
+}
+int multiply(int num5, int num6) {
+    return num5 * num6;
+}
+int divide(int num5, int num6) {
+    return num5 / num6;
+}
